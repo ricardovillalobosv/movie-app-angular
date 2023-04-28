91fcc29736791aed62e5d979de6361d1504fe47f
Ricardo Villalobos Vargas <ricardo_villalobosv@hotmail.com>
2023-04-28 00:35:36 -0500

    chg -styles card -movies page

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+export interface Movie {
+  title?: string;
+  original_title?: string;
+  poster_path?: string;
+  release_date?: string;
+  vote_average?: string;
+}
 
 @Component({
   selector: 'card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent {
+  @Input() movie: Movie = {};
 
+  get img() {
+    return `https://image.tmdb.org/t/p/w200/${this.movie.poster_path}`;
+  }
 }
